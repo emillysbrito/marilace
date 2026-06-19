1d00c843bbd561eefc3336b63886e7b62fb221fb
Emilly Brito <emillydesousabrito@gmail.com>
2026-06-19 11:34:36 -0300

    docs: adiciona documentação relacionada ao Trabalho de Conclusão de Curso

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -5,10 +5,32 @@ import { FooterAnon } from '../components/FooterAnon'
 import { Link } from 'react-router-dom'
 import { Btn } from '../components/Btn'
 import { TbUserPlus } from "react-icons/tb";
+import { z } from "zod";
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 import imgLogin from '../assets/img/colagem-cadastro.png'
 
+const userSchema =  z.object({
+    email: z.string().min(1, "O e-mail é obrigatório").email("E-mail inválido"),
+    senha: z.string().min(6, "A senha deve ter pelo menos 6 caracteres")
+});
+
+    type UserFormData = z.infer<typeof userSchema>;
+
+
 export function Login(){
+    const {
+        register, 
+        handleSubmit,
+        formState: { errors }, 
+    } = useForm<UserFormData>({
+        resolver: zodResolver(userSchema),
+});
+    function onSubmit(data: UserFormData) {
+        console.log(data);
+    }
+
     return(
         <div className={ styles.login }>
             <HeaderAnon/>
@@ -22,15 +44,25 @@ export function Login(){
                     <form className={ styles.formLogin }>                       
                         <div className={ styles.inputContainer }>
                             <label htmlFor="email">E-mail:</label>
-                            <input id='email' type="email" />
+                            <input 
+                                id='email' 
+                                type="email" 
+                                {...register("email")}
+                            />
+                            {errors.email && <p>{errors.email.message}</p>}
                         </div>
 
                         <div className={ styles.inputContainer }>
                             <label htmlFor="senha">Senha:</label>
-                            <input id='senha' type="password" />
+                            <input 
+                                id='senha' 
+                                type="password" 
+                                {...register("senha")}
+                            />
+                            {errors.senha && <p>{errors.senha.message}</p>}
                         </div>
 
-                        <Btn route="/" text="Registrar"/>
+                        <Btn route="/" text="Registrar"/> 
 
                     </form>
                     <Link to={'/register'}
