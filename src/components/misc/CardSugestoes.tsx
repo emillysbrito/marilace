5d80dd9c5d1fd0a1be9cc13dba88b959fd10a991
Emilly Brito <emillydesousabrito@gmail.com>
2026-07-27 20:56:51 -0300

    feat: adiciona componente CardSugestoes

---
+++ src/components/misc/CardSugestoes.tsx
@@ -0,0 +1,44 @@
+import styles from './CardSugestoes.module.css'
+import { CardPerfil } from './CardPerfil'
+
+export function CardSugestoes(){
+    return(
+        <div className={ styles.container }>
+            <h1>Sugestões para seguir</h1>
+
+            <div className={ styles.containerCards }>
+                <CardPerfil
+                nome='Ana Clara'
+                username='anaclara'
+                verificado
+                emblemaS
+                emblemaT
+                />
+
+                <CardPerfil
+                nome='Emilly Brito'
+                username='emillybrito'
+                verificado
+                emblemaS
+                emblemaE
+                />
+
+                <CardPerfil
+                nome='Fernanda Leal'
+                username='fernandaleal'
+                verificado
+                emblemaT
+                />
+
+                <CardPerfil
+                nome='Guilherme Martins'
+                username='guilhermemartins'
+                verificado
+                emblemaT
+                />
+            </div>
+            <hr className={ styles.linha} />
+            <button className={ styles.btnVerTodos }>Ver todos</button>
+        </div>
+    )
+}
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,18 +1,12 @@
 import styles from './Profile.module.css'
 import { HeaderUser } from '../components/headers/HeaderUser'
-import { CardPerfil } from '../components/misc/CardPerfil'
+import { CardSugestoes } from '../components/misc/CardSugestoes'
 
 export function Profile(){
     return (
         <div className={ styles.profile }>
             <HeaderUser/>
-            <CardPerfil
-            nome='Ana Clara'
-            username='ana'
-            verificado
-            emblemaS
-            emblemaT
-            />
+            <CardSugestoes/>
         </div>
     )
 }
